63bff47001c33e656781cc7546675af1d0784223
Crowdbotics <team@crowdbotics.com>
2023-04-05 21:50:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-haze-39735",
+  url: "https://shiny-haze-39735.botics.co",
+  api: "https://shiny-haze-39735.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
